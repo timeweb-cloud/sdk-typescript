f040e7f2911e463f95a6b33a69393882a8d1da25
timeweb.cloud <dev@timeweb.cloud>
2024-07-15 13:20:40 +0000

    Updated: 2024-07-15

--- src/apis/ServersApi.ts
+++ src/apis/ServersApi.ts
@@ -238,10 +238,6 @@ export interface ImageUnmountAndServerReloadRequest {
     serverId: number;
 }
 
-export interface InstallServerRequest {
-    serverId: number;
-}
-
 export interface PerformActionOnBackupOperationRequest {
     serverId: number;
     diskId: number;
@@ -1390,45 +1386,6 @@ export class ServersApi extends runtime.BaseAPI {
      */
     async imageUnmountAndServerReload(requestParameters: ImageUnmountAndServerReloadRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<void> {
         await this.imageUnmountAndServerReloadRaw(requestParameters, initOverrides);
-    }
-
-    /**
-     * Чтобы установить сервер, отправьте POST-запрос на `/api/v1/servers/{server_id}/install`.
-     * Установка сервера
-     */
-    async installServerRaw(requestParameters: InstallServerRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<runtime.ApiResponse<void>> {
-        if (requestParameters.serverId === null || requestParameters.serverId === undefined) {
-            throw new runtime.RequiredError('serverId','Required parameter requestParameters.serverId was null or undefined when calling installServer.');
-        }
-
-        const queryParameters: any = {};
-
-        const headerParameters: runtime.HTTPHeaders = {};
-
-        if (this.configuration && this.configuration.accessToken) {
-            const token = this.configuration.accessToken;
-            const tokenString = await token("Bearer", []);
-
-            if (tokenString) {
-                headerParameters["Authorization"] = `Bearer ${tokenString}`;
-            }
-        }
-        const response = await this.request({
-            path: `/api/v1/servers/{server_id}/install`.replace(`{${"server_id"}}`, encodeURIComponent(String(requestParameters.serverId))),
-            method: 'POST',
-            headers: headerParameters,
-            query: queryParameters,
-        }, initOverrides);
-
-        return new runtime.VoidApiResponse(response);
-    }
-
-    /**
-     * Чтобы установить сервер, отправьте POST-запрос на `/api/v1/servers/{server_id}/install`.
-     * Установка сервера
-     */
-    async installServer(requestParameters: InstallServerRequest, initOverrides?: RequestInit | runtime.InitOverrideFunction): Promise<void> {
-        await this.installServerRaw(requestParameters, initOverrides);
     }
 
     /**
